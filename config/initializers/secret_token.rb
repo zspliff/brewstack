48bb60bfbd738c3e3d9f503fda99cf7e70812ad7
Zed Marji <zspliff@gmail.com>
2012-05-27 21:48:54 -0700

    Brewstack start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brewstackproj::Application.config.secret_token = 'c098a73d1cc017de8ef70e809c721dbe7034b0b2e7a3717a4e86557114ca67f7f9884a593bd03030f10208ffdb7cd7eb06f3231af23d5b9f879e46ab1739b5d4'
